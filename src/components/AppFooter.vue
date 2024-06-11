5780f6f71a09f93f7e6b961f91113a709e10ec95
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-11 12:11:17 +0200

    upd style footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,38 +1,54 @@
 <template>
     <footer>
-        <ul>
-            <li>CONTACT INFO</li>
-            <li>2/45 Tower Street, New York
-                USA</li>
-            <li>Call Us On 0800 840 1010</li>
-            <li>Demo@Example.Com</li>
-        </ul>
-        <ul>
-            <li>INFORMATION</li>
-            <li>Product Support</li>
-            <li>Checkout</li>
-            <li>Report Abuse</li>
-            <li>Redeem Voucher</li>
-            <li>Order Status</li>
-        </ul>
-        <ul>
-            <li>USEFUL LINKS</li>
-            <li>Policies & Rules</li>
-            <li>Privacy Policy</li>
-            <li>License Policy</li>
-            <li>My Account</li>
-            <li>Locality</li>
-        </ul>
-        <ul>
-            <li>NEWSLETTER</li>
-            <li>Subscribe To Receive Inspiration,
-                Ideas & News In Your Inbox.</li>
-            <li><input type="text" placeholder="Enter Your Email"></li>
-            <li><button>SUBSCRIBE</button></li>
-        </ul>
+        <div class="constainer-fluid">
+            <div class="row">
+
+                <ul class="col-3">
+                    <li><strong>CONTACT INFO</strong></li>
+                    <li>2/45 Tower Street, New York
+                        USA</li>
+                    <li>Call Us On 0800 840 1010</li>
+                    <li>Demo@Example.Com</li>
+                </ul>
+                <ul class="col-3">
+                    <li><strong>INFORMATION</strong></li>
+                    <li>Product Support</li>
+                    <li>Checkout</li>
+                    <li>Report Abuse</li>
+                    <li>Redeem Voucher</li>
+                    <li>Order Status</li>
+                </ul>
+                <ul class="col-3">
+                    <li><strong>USEFUL LINKS</strong></li>
+                    <li>Policies & Rules</li>
+                    <li>Privacy Policy</li>
+                    <li>License Policy</li>
+                    <li>My Account</li>
+                    <li>Locality</li>
+                </ul>
+                <ul class="col-3">
+                    <li><strong>NEWSLETTER</strong></li>
+                    <li>Subscribe To Receive Inspiration,
+                        Ideas & News In Your Inbox.</li>
+                    <li><input type="text" placeholder="Enter Your Email"></li>
+                    <li><button>SUBSCRIBE</button></li>
+                </ul>
+            </div>
+        </div>
+        
     </footer>
 </template>
-<style scoped></style>
+<style scoped>
+li{
+    list-style: none;
+    padding: 10px;
+}
+footer{
+    background-color: black;
+    color: white;
+    padding: 20px;
+}
+</style>
 <script>
 export default {
     name: 'AppFooter'
