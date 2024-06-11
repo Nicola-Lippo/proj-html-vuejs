22a43bfa8173a3073ca5baf713ec575e68adc945
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-11 15:55:53 +0200

    add text area

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -4,19 +4,36 @@
     }
 </script>
 <template>
-    <div class="container">
+    <div class="container m-5">
         <div class="row">
             <div class="col-6">
-                <div class="form-floating mb-3">
-  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-  <label for="floatingInput">Email address</label>
-</div>
-<div class="form-floating">
-  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-  <label for="floatingPassword">Password</label>
-</div>
+                <div class="form-floating">
+                    <p>Your name</p> 
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    
+                </div>
+                <div class="form-floating">
+                    <p>Your email</p>
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    
+                </div>
+                <div class="form-floating">
+                    <p>Subject</p>
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    
+                </div>
+                <div class="form-floating">
+                    <p>Your message (optional)</p>
+                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                    
+                </div>
             </div>
             <div class="col-6">destra</div>
         </div>
     </div>
 </template>
+<style scoped>
+.form-floating{
+    padding-top: 10px;
+}
+</style>
