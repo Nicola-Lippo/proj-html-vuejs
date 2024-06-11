e3875c3f984bb6eb10f44f54e2abe5cc93a01fb8
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-11 17:35:00 +0200

    fix map

--- src/components/AppMap.vue
+++ src/components/AppMap.vue
@@ -4,5 +4,14 @@ export default {
 }
 </script>
 <template>
-    <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=London%20Eye%2C%20London%2C%20United%20Kingdom&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near" title="London Eye, London, United Kingdom" aria-label="London Eye, London, United Kingdom"></iframe>
+    <div class="container-fluid mt-5 mb-5">
+
+        <iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=London%20Eye%2C%20London%2C%20United%20Kingdom&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near" title="London Eye, London, United Kingdom" aria-label="London Eye, London, United Kingdom"></iframe>
+    </div>
 </template>
+<style>
+iframe{
+    width: 100%;
+    height: 700px;
+}
+</style>
