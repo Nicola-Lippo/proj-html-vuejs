0f52b75cae4d94b46bb7df1cb600a86d44c32b21
antoniozacca <antoniozaccaria057@gmail.com>
2024-06-11 14:23:07 +0200

    fix img

--- src/pages/AppContacts.vue
+++ src/pages/AppContacts.vue
@@ -5,7 +5,28 @@ export default {
 </script>
 
 <template>
-<div><img src="./img/bredcum-img.jpg"></div>
+    <div class="img">
+        <div class="grandezza">
+            <h1>contacts</h1>
+            <p>Home / Contacts</p>
+        </div>
+            
+    </div>
 </template>
 
-<style></style>
+<style>
+.img{
+    background-image: url(./img/bredcum-img.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-attachment: scroll;
+}
+h1, p{
+    text-align: center;
+    color: white;
+}
+.grandezza{
+    min-height: 250px;
+}
+</style>
