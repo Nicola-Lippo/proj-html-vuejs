9398e6825345b7fd2de4cc2b3ed6d12dcc4a089c
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-06-12 10:49:17 +0200

    [upd] new component by promps

---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'CardList',
+    props: ['foto', 'titolo', 'tipo']
+}
+</script>
+
+<template>
+    <div class="m-2">
+        <img :src="foto" :alt="img">
+        <div class="p-3"><strong>{{ titolo }}</strong></div>
+        <p>{{ tipo }}</p>
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 100%;
+}
+</style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,18 +1,21 @@
 <script>
 import pelicula from '/src/components/pelicula.vue';
 import AppImgMc from '../components/AppImgMc.vue';
+import YourWatchlist from '../components/YourWatchlist.vue';
 export default {
     name: 'AppHome',
     components: {
         pelicula,
-        AppImgMc
+        AppImgMc,
+        YourWatchlist
     }
-}
+};
 </script>
 
 <template>
     <pelicula />
     <AppImgMc />
+    <YourWatchlist />
 </template>
 
 <style></style>
